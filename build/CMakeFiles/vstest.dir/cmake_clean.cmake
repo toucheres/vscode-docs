8b92f7a71ba81c46006f6abb8cac7226c5ba19cb
Jiahang He <761844639@qq.com>
2024-12-29 16:35:54 +0000

    first

---
+++ build/CMakeFiles/vstest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vstest.dir/main.cpp.o"
+  "vstest.pdb"
+  "vstest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vstest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
